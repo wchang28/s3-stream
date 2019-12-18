6dc5904f10c0e3a4d5738056c62a79818a40e5cf
Wen Chang <wchang@cerberus.com>
2019-12-18 15:35:07 -0500

    remove old code

--- dist/index.js
+++ dist/index.js
@@ -45,30 +45,4 @@ var Put = /** @class */ (function (_super) {
     return Put;
 }(io_stream_templates_1.WritableTemplate));
 exports.Put = Put;
-/*
-export class Put extends Writable {
-    private _pt: PassThrough;
-    constructor(s3: S3, params: S3.PutObjectRequest) {
-        super();
-        this._pt = new PassThrough();
-        this._pt.on("error", (err: any) => {
-            this.emit("error", err);
-        });
-        params.Body = this._pt;
-        s3.upload(params, (err: any, data: S3.ManagedUpload.SendData) => {
-            this.emit("upload-complete", err, data);
-            if (err) {
-                this.emit("error", err);
-            }
-        });
-        this.on("finish", () => {
-            // end() was called on this object
-            this._pt.end(); // end the write side of PassThrough, which will trigger the 'end' event on the read side of PassThrough
-        });
-    }
-    _write(chunk: any, encoding: string, callback: (error?: Error | null) => void) {
-        this._pt.write(chunk, encoding, callback);
-    }
-}
-*/ 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -26,30 +26,3 @@ export class Put extends WritableTemplate {
         });
     }
 }
-
-/*
-export class Put extends Writable {
-    private _pt: PassThrough;
-    constructor(s3: S3, params: S3.PutObjectRequest) {
-        super();
-        this._pt = new PassThrough();
-        this._pt.on("error", (err: any) => {
-            this.emit("error", err);
-        });
-        params.Body = this._pt;
-        s3.upload(params, (err: any, data: S3.ManagedUpload.SendData) => {
-            this.emit("upload-complete", err, data);
-            if (err) {
-                this.emit("error", err);
-            }
-        });
-        this.on("finish", () => {
-            // end() was called on this object
-            this._pt.end(); // end the write side of PassThrough, which will trigger the 'end' event on the read side of PassThrough
-        });
-    }
-    _write(chunk: any, encoding: string, callback: (error?: Error | null) => void) {
-        this._pt.write(chunk, encoding, callback);
-    }
-}
-*/
